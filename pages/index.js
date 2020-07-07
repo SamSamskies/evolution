64a17dee726ccdf838db06e01d612e508d1d3316
karleee <karleee@protonmail.com>
2020-07-06 19:03:19 -0700

    Add styling to homepage

---
+++ pages/index.js
@@ -0,0 +1,96 @@
+import Head from 'next/head'
+
+// Importing stylesheets
+import globalStyles from '../styles/global'
+
+export default function Home() { 
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div className="header">
+          <div className="intro">
+            <h1 className="title">
+              Welcome to <a href="https://github.com/SamSamskies/evolution" target="_blank">Evolution!</a>
+            </h1>
+
+            <p className="description">This is a fun idea put forth by Sam for a group of developers.</p>
+            <p className="description">Get started by checking out his GitHub and let's work together to see what we can build.</p>
+          </div>
+
+          <div className="contributors">
+            {/* Add your name and Github here! */}
+            <h2>Developers</h2>
+            <div className="names">
+              <a href="https://github.com/SamSamskies" target="_blank">Sam</a>
+              <a href="https://github.com/MarkWillisford" target="_blank">Mark</a>
+              <a href="https://github.com/itfibonacci" target="_blank">Levon</a>
+              <a href="" target="_blank">Karen</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid">
+          <a href="https://nextjs.org/docs" className="card" target="_blank">
+            <div className="card-header">
+              <div className="image"></div>
+              <h3>Documentation &rarr;</h3>
+            </div>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className="card" target="_blank">
+            <div className="card-header">
+              <div className="image"></div>
+              <h3>Learn &rarr;</h3>
+            </div>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className="card"
+            target="_blank">
+            <div className="card-header">
+              <div className="image"></div>
+              <h3>Examples &rarr;</h3>
+            </div>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className="card"
+            target="_blank">
+            <div className="card-header">
+              <div className="image"></div>
+              <h3>Deploy &rarr;</h3>
+            </div>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a>
+      </footer>
+
+      <style jsx global>
+        {globalStyles}
+      </style>
+    </div>
+  )
+}
---
+++ styles/global.js
@@ -0,0 +1,204 @@
+// styles/global.js
+import css from 'styled-jsx/css'
+
+export default css.global`
+  :root {
+    --dark-gray: #282828;
+    --mid-gray: #535353;
+    --gray: #757580;
+    --light-gray: #EBEBFF;
+    --orange: #FF8119;
+    --pink: #F25CA2;
+    --blue: #0B9ED9;
+  }
+
+  html, body, div, h1, h2, h3, p, a, ul {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+      Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+      sans-serif;
+  }
+
+  html {
+    background: white;
+    box-sizing: border-box;
+    padding: 2% 5%;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  footer {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    border-top: 1px solid var(--light-gray);
+    width: 100%;
+    margin-top: 7%;
+    box-sizing: border-box;
+    padding-top: 1%;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  h1, h2 {
+    margin-bottom: 5%;
+  }
+
+  h1 {
+    font-size: 24px;
+    color: var(--dark-gray);
+  }
+
+  h3 {
+    font-size: 16px;
+    font-weight: 600;
+    color: var(--gray);
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+    box-sizing: border-box;
+    padding: 1% 2%;
+  }
+
+  a:hover {
+    cursor: pointer;
+  }
+
+  p {
+    color: var(--mid-gray);
+  }
+
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    width: 100%;
+    margin-top: 3%;
+  }
+
+  .intro {
+    box-sizing: border-box;
+    padding: 2%;
+    width: 27%;
+    margin-right: 5%;
+    border: 3px solid black;
+    box-shadow: 10px 10px 0 var(--pink);
+  }
+
+  .intro a:hover {
+    color: var(--blue);
+    transition: color 0.25s;
+  }
+
+  .intro p {
+    line-height: 1.5;
+  }
+
+  .intro p:first-of-type {
+    margin-bottom: 4%;
+  }
+
+  .contributors {
+    width: 30%;
+    margin-right: 0;
+  }
+
+  .contributors h2 {
+    font-size: 16px;
+    font-weight: 700;
+    color: var(--gray);
+  }
+
+  .names {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    list-style: none;
+  }
+
+  .names a {
+    box-sizing: border-box;
+    padding: 2% 5%;
+    background: var(--light-gray);
+    border-radius: 3px;
+    font-weight: 500;
+    color: var(--mid-gray);
+    margin: 0 3% 2% 0;
+  }
+
+  .names a:hover {
+    background: var(--blue);
+    border-radius: 3px;
+    transition: background 0.5s;
+    color: white;
+  }
+
+  .grid {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 65%;
+    margin-top: 5%;
+  }
+
+  .card {
+    box-sizing: border-box;
+    padding: 3%;
+    width: 47%;
+    margin: 2% 3% 2% 0;
+    border: 2px solid black;
+  }
+
+  .card:hover,
+  .card:focus,
+  .card:active {
+    border-color: var(--blue);
+  }
+
+  .card-header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 3%;
+  }
+
+  .card h3 {
+    font-size: 1.2rem;
+    color: var(--pink);
+  }
+
+  .card .image {
+    width: 2em;
+    height: 2em;
+    background: orange;
+    margin-right: 3%;
+  }
+
+  .card p {
+    font-size: 1rem;
+    line-height: 1.5;
+    color: var(--gray);
+  }
+
+  .logo {
+    height: 0.8rem;
+  }
+
+  @media (max-width: 600px) {
+    .grid {
+      width: 100%;
+      flex-direction: column;
+    }
+  }
+`
