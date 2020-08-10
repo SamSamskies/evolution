00d44dd2190c57d66003fb73507e081f905726a8
Hannah L <54752231+hannahlivnat@users.noreply.github.com>
2020-08-09 20:40:14 -0700

    Evolutioncontribution hannahlivnat - navbar (#11)
    * added a navbar and adjusted name styling to fix text overlap for longer names
    
    * moved header into its own component and separated out css files into components
    
    * deleted commented out padding for html
    
    * fixed double cards
    
    * formatted using prettier

--- components/Contributors.js
+++ components/Contributors.js
@@ -1,33 +1,36 @@
+import styles from "./css/Contributors.module.css";
+
 export default function Contributors() {
-  /*
+    /*
 
   Add your name and Github here!
 
   */
-  const people = [
-    { name: "Sam", githubLink: "https://github.com/SamSamskies" },
-    { name: "Mark", githubLink: "https://github.com/MarkWillisford" },
-    { name: "Levon", githubLink: "https://github.com/itfibonacci" },
-    { name: "Karen", githubLink: "https://github.com/karleee" },
-    { name: "James", githubLink: "https://github.com/ngjamesng" },
-    { name: "Megan", githubLink: "https://github.com/mcdevittbass" },
-    { name: "Emily", githubLink: "https://github.com/eaquin1" },
-  ];
+    const people = [
+        { name: "Sam", githubLink: "https://github.com/SamSamskies" },
+        { name: "Mark", githubLink: "https://github.com/MarkWillisford" },
+        { name: "Levon", githubLink: "https://github.com/itfibonacci" },
+        { name: "Karen", githubLink: "https://github.com/karleee" },
+        { name: "James", githubLink: "https://github.com/ngjamesng" },
+        { name: "Hannah", githubLink: "https://github.com/hannahlivnat" },
+        { name: "Megan", githubLink: "https://github.com/mcdevittbass" },
+        { name: "Emily", githubLink: "https://github.com/eaquin1" },
+    ];
 
-  const ContributorLink = ({ person }) => (
-    <a href={person.githubLink} target="_blank">
-      {person.name}
-    </a>
-  );
+    const ContributorLink = ({ person }) => (
+        <a href={person.githubLink} target="_blank">
+            {person.name}
+        </a>
+    );
 
-  return (
-    <div className="contributors">
-      <h2>Developers</h2>
-      <div className="names">
-        {people.map((p) => (
-          <ContributorLink person={p} key={p.githubLink} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.contributors} id="contributor-section">
+            <h2>Developers</h2>
+            <div className={styles.names}>
+                {people.map((p) => (
+                    <ContributorLink person={p} key={p.githubLink} />
+                ))}
+            </div>
+        </div>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,23 @@
 import Head from "next/head";
-import Contributors from "../components/Contributors";
+import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Cards from "../components/Cards";
+
+// Importing stylesheets
 import globalStyles from "../styles/global";
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Evolution</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <div className="header">
-          <div className="intro">
-            <h1 className="title">
-              Welcome to{" "}
-              <a
-                href="https://github.com/SamSamskies/evolution"
-                target="_blank"
-              >
-                Evolution!
-              </a>
-            </h1>
-            <p className="description">
-              This is a fun idea put forth by Sam for a group of developers.
-            </p>
-            <p className="description">
-              Get started by checking out his GitHub and let's work together to
-              see what we can build.
-            </p>
-          </div>
-          {/* See Contributors component to add your name! */}
-          <Contributors />
+    return (
+        <div className="container">
+            <Head>
+                <title>Evolution</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <Footer />
+            <style jsx global>
+                {globalStyles}
+            </style>
         </div>
-      </main>
-      <Footer />
-      <style jsx global>
-        {globalStyles}
-      </style>
-    </div>
-  );
+    );
 }
