004ff1e756cfe479ee743df7d0a7eaac9c2d37d9
Emily Aquin <36141143+eaquin1@users.noreply.github.com>
2020-08-09 19:57:16 -0500

    added prettier as a dev dependency and a format npm script (#13)
    * added prettier as a dev dependency and a format npm script
    
    * removed package-lock.json and installed prettier with yarn
    
    * added my name
    
    * ran prettier

--- components/Contributors.js
+++ components/Contributors.js
@@ -1,8 +1,8 @@
 export default function Contributors() {
-  /* 
-  
-  Add your name and Github here! 
-  
+  /*
+
+  Add your name and Github here!
+
   */
   const people = [
     { name: "Sam", githubLink: "https://github.com/SamSamskies" },
@@ -10,18 +10,23 @@ export default function Contributors() {
     { name: "Levon", githubLink: "https://github.com/itfibonacci" },
     { name: "Karen", githubLink: "https://github.com/karleee" },
     { name: "James", githubLink: "https://github.com/ngjamesng" },
-    { name: "Megan", githubLink: "https://github.com/mcdevittbass"}
+    { name: "Megan", githubLink: "https://github.com/mcdevittbass" },
+    { name: "Emily", githubLink: "https://github.com/eaquin1" },
   ];
 
   const ContributorLink = ({ person }) => (
-    <a href={person.githubLink} target="_blank">{person.name}</a>
-  )
+    <a href={person.githubLink} target="_blank">
+      {person.name}
+    </a>
+  );
 
   return (
     <div className="contributors">
       <h2>Developers</h2>
       <div className="names">
-        {people.map(p => <ContributorLink person={p} key={p.githubLink} />)}
+        {people.map((p) => (
+          <ContributorLink person={p} key={p.githubLink} />
+        ))}
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Contributors from '../components/Contributors';
+import Head from "next/head";
+import Contributors from "../components/Contributors";
 import Footer from "../components/Footer";
-import globalStyles from '../styles/global'
+import globalStyles from "../styles/global";
 
 export default function Home() {
   return (
@@ -14,10 +14,21 @@ export default function Home() {
         <div className="header">
           <div className="intro">
             <h1 className="title">
-              Welcome to <a href="https://github.com/SamSamskies/evolution" target="_blank">Evolution!</a>
+              Welcome to{" "}
+              <a
+                href="https://github.com/SamSamskies/evolution"
+                target="_blank"
+              >
+                Evolution!
+              </a>
             </h1>
-            <p className="description">This is a fun idea put forth by Sam for a group of developers.</p>
-            <p className="description">Get started by checking out his GitHub and let's work together to see what we can build.</p>
+            <p className="description">
+              This is a fun idea put forth by Sam for a group of developers.
+            </p>
+            <p className="description">
+              Get started by checking out his GitHub and let's work together to
+              see what we can build.
+            </p>
           </div>
           {/* See Contributors component to add your name! */}
           <Contributors />
@@ -27,6 +38,6 @@ export default function Home() {
       <style jsx global>
         {globalStyles}
       </style>
-    </div> 
-  )
+    </div>
+  );
 }
