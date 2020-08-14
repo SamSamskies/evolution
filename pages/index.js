43976d52cf79e1983d8e295e56f3299bb984fbba
Sam Samskies <samsamskies@gmail.com>
2020-08-14 11:56:38 -0700

    Remove unnecessary container div

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,11 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Page from "../components/Page";
 
-
-
 export default function Home() {
   return (
-    <div className="container">
-      <Page>
-        <Header />
-        <Footer />
-      </Page>
-    </div>
+    <Page>
+      <Header />
+      <Footer />
+    </Page>
   );
 }
